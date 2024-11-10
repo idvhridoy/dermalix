1e9cff139f1f7e9666852c4deeccd66913a11134
idvhridoy <idvhridoy@gmail.com>
2024-11-10 20:07:53 +0600

    image link replaced

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -107,7 +107,7 @@ const megaMenuContent: MegaMenuData = {
     featured: [
       {
         title: 'Our Lab',
-        image: 'https://images.unsplash.com/photo-1614859324967-bdf413c35a5b',
+        image: 'https://images.unsplash.com/photo-1582719471327-5bd41fcf7f7f?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         href: '/about/lab'
       }
     ]
